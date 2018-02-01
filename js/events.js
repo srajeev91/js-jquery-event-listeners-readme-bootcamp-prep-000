2580971eaa8d366c7aedf4fa03d8e44427ae5b70
Sangeetha Rajeev <sang.rajeev@gmail.com>
2018-02-01 02:50:13 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -17,3 +17,9 @@ function frameIt() {
     $('img').addClass('tasty');
   });
 }
+
+function submitIt() {
+  $("form").on("submit", function() {
+    alert("Your form is going to be submitted now.");
+  });
+}
